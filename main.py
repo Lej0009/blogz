d9d197b59db650a120d254ffdecf59d9dac4b818
Lauren Jahera <lauren.jahera@gmail.com>
2019-10-16 12:28:09 -0400

    create page templates, main page needs to show blog posts

--- main.py
+++ main.py
@@ -4,49 +4,58 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:1234@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'y37kGcys&zP3B'
 
-
 class Blog(db.Model):
-    
+
     id = db.Column(db.Integer, primary_key=True)
-    blog = db.Column(db.String(120))
-    published = db.Column(db.Boolean)
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __init__(self, name, owner):
-        self.blog=blog
-        self.published=False
-        self.owner = owner
+    name = db.Column(db.String(120))
+    body = db.Column(db.String(800))
+    completed = db.Column(db.Boolean)
 
 
-
-
+    def __init__(self, blog, blogbody):
+        self.name=blog
+        self.blogbody=blogbody
+        self.completed=False
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
-    owner = User.query.filter_by(email=session['email']).first()
+ 
+    blogs = Blog.query.filter_by(completed=False).all()
+    completed_blogs = Blog.query.filter_by(completed=True).all()
+    return render_template('blogs.html',title="Build-A-Blog!", 
+        completed_blogs=completed_blogs)
+
+
+# @app.route('/delete-blog', methods=['POST'])
+# def delete_blog():
+
+#     blog_id = int(request.form['blog-id'])
+#     blog = Blog.query.get(blog_id)
+#     blog.completed = True
+#     db.session.add(blog)
+#     db.session.commit()
+
+#     return redirect('/')
+
+@app.route('/newblog', methods=['POST', 'GET'])
+def new_blog():
 
     if request.method == 'POST':
-        blog_name = request.form['blog']        
-        new_blog = Blog(blog_name, owner)
+        blog_title = request.form['blog']   
+        blog_body = request.form['blogbody']     
+        new_blog = Blog(blog_title, blog_body)
         db.session.add(new_blog)
         db.session.commit()
 
-    blogs = Task.query.filter_by(published=False,owner=owner).all()
-    published_blogs = Task.query.filter_by(published=True,owner=owner).all()
-    return render_template('blogs.html',title="Build-A-Blog!", 
-        blogs=blogs, published_blogs=published_blogs)
-
-
-
-
-
-
-
+        return redirect('/')
+    else:
+        return render_template('newblog.html')
 
 if __name__ == '__main__':
     app.run()
