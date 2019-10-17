53febd8c12ee155ac95a23ce76c93f487ebe8cf2
Lauren Jahera <lauren.jahera@gmail.com>
2019-10-17 16:07:29 -0400

    add login and register functionality

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:1234@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:1234@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'y37kGcys&zP3B'
@@ -27,18 +27,80 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     body = db.Column(db.String(800))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
-
-    def __init__(self, blog, blogbody):
+    def __init__(self, blog, blogbody, owner):
         self.name=blog
         self.body=blogbody
+        self.owner=owner
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, email, password):
+        self.email=email
+        self.password=password
+
+@app.before_request
+def require_login():
+    allowed_routes = ['login', 'register']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect('/login') 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(email=email).first()
+        if user and user.password == password:
+            session['email']  =email
+            flash("Logged in")
+            return redirect('/')
+        else:
+            flash("User password incorrect or user does not exist", 'error')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # TODO - validate user's data
+
+        existing_user = User.query.filter_by(email=email).first()
+        if not existing_user:
+            new_user = User(email, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['email'] = email
+            return redirect('/')
+        else:
+            # TODO - user better response messaging
+            return '<h1>Duplicate user</h1>'
+
+    return render_template('register.html')
+
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect('/')
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
 
+    owner = User.query.filter_by(email=session['email']).first()
+
     blog_id = str(request.args.get('id'))
-    blogs = Blog.query.all()
+    blogs = Blog.query.filter_by(owner=owner).all()
     myblog = Blog.query.get(blog_id)
 
     return render_template('blogs.html', blogs=blogs, myblog=myblog)
@@ -62,7 +124,8 @@ def new_blog():
             return render_template('newblog.html', title_error=title_error_msg, body_error=body_error_msg, blog_title=blog_title)
                    
         else:
-            new_blog = Blog(blog_title, blog_body)
+            owner = User.query.filter_by(email=session['email']).first()
+            new_blog = Blog(blog_title, blog_body, owner)
             db.session.add(new_blog)
             db.session.commit()  
             return redirect('/?id=' + str(new_blog.id)) 
